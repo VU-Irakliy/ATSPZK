c67aa5850cd4d3126262c9d61ab1cfc69b397841
VU-Irakliy <i.marsagishvili@student.vu.nl>
2022-04-03 16:16:16 +0200

    comments

--- searchbound.py
+++ searchbound.py
@@ -1,5 +1,16 @@
 
 
+#####
+"""
+The input will be unsorted/sorted?
+When using a tree value, to retrieve a value, do tree.data
+if left/right, do tree.right/left
+how the tree will be made?
+look at the image made on a meeting u moron
+but assignment problem...
+WTFFFFFF.>>>>
 
+
+"""
 def depthFirstBranchAndBound(tree):
     ...
