7483a63d5095435e5a30eecf9566dfc5028158ec
VU-Irakliy <i.marsagishvili@student.vu.nl>
2022-04-06 13:41:25 +0200

    a bit of progress

--- main.py
+++ main.py
@@ -1,72 +1,63 @@
 from searchbound import *
 
-
+#INSIRATION: https://www.geeksforgeeks.org/given-linked-list-representation-of-complete-tree-convert-it-to-linked-representation/
 class Node:
  
-        # Constructor to create a new node
         def __init__(self, data):
             self.data = data
             self.next = None
+        
+        def insert(root, item):
+            temp = Node(item)
+            
+            if (root == None):
+                root = temp
+            else :
+                ptr = root
+                while (ptr.next != None):
+                    ptr = ptr.next
+                ptr.next = temp
+            
+            return root
  
-# Binary Tree Node structure
 class BinaryTreeNode:
  
-    # Constructor to create a new node
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
  
-# Class to convert the linked list to Binary Tree
 class Conversion:
  
-    # Constructor for storing head of linked list
-    # and root for the Binary Tree
+    
     def __init__(self, data = None):
         self.head = None
         self.root = None
  
     def push(self, new_data):
  
-        # Creating a new linked list node and storing data
         new_node = Node(new_data)
- 
-        # Make next of new node as head
         new_node.next = self.head
- 
-        # Move the head to point to new node
         self.head = new_node
  
     def convertList2Binary(self):
  
-        # Queue to store the parent nodes
         q = []
  
-        # Base Case
         if self.head is None:
             self.root = None
             return
  
-        # 1.) The first node is always the root node,
-        # and add it to the queue
         self.root = BinaryTreeNode(self.head.data)
         q.append(self.root)
  
-        # Advance the pointer to the next node
         self.head = self.head.next
  
-        # Until the end of linked list is reached, do:
         while(self.head):
  
-            # 2.a) Take the parent node from the q and
-            # and remove it from q
             parent = q.pop(0) # Front of queue
  
-            # 2.c) Take next two nodes from the linked list.
-            # We will add them as children of the current
-            # parent node in step 2.b.
-            # Push them into the queue so that they will be
-            # parent to the future node
+            
             leftChild= None
             rightChild = None
  
@@ -78,14 +69,20 @@ class Conversion:
                 q.append(rightChild)
                 self.head = self.head.next
  
-            #2.b) Assign the left and right children of parent
+            # Assign the left and right children of parent
             parent.left = leftChild
             parent.right = rightChild
     
 def fillTheTree(input):
-    ...
+    if input[0] == None:
+        return
+    theRootNode = Node(input[0])
+    print(theRootNode.data)
+    #####HOW TO CONSTRUCT THIS>>  
+
 
 def main():
-    ...
+    input = [1,4,8,14,55,21,44,61]
+    theTree = fillTheTree(input)
 
 main()
